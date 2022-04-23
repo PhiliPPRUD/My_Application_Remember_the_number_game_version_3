7fd88e4b576651fc5128411adab961beeae982df
Philipp2022RUD <77397702+Philipp2022RUD@users.noreply.github.com>
2022-04-23 15:18:24 +0300

    Третья версия с небольшими изменениями

--- app/src/main/java/my/application/myapplicationrememberthenumbergame/MainActivity.java
+++ app/src/main/java/my/application/myapplicationrememberthenumbergame/MainActivity.java
@@ -3,6 +3,7 @@ package my.application.myapplicationrememberthenumbergame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView result_info;
     private int vvodInt;
     private int rInt;
+    private Button further_btn;
+    private int seconds;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +31,36 @@ public class MainActivity extends AppCompatActivity {
         user_field = findViewById(R.id.user_field);
         main_btn = findViewById(R.id.main_btn);
         result_info = findViewById(R.id.result_info);
+        further_btn = findViewById(R.id.further_btn);
+        seconds = 2;
 
-        rInt = (int) ((Math.random() * ((999-100)+1))+100);
-        info.setText(Integer.toString(rInt));
+
+        further_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                rInt = (int) ((Math.random() * ((100000-1000)+1))+1000);
+                info.setText(Integer.toString(rInt));
+
+                CountDownTimer my_timer = new CountDownTimer(seconds*1000,1000) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        info.setText(Integer.toString(rInt));
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        info.setText("");
+                    }
+
+                };
+                my_timer.start();
+
+
+            }
+        });
+
 
         main_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
                 vvodInt = Integer.parseInt(user_field.getText().toString());
                 if(rInt==vvodInt) {
                     result_info.setText("Верно");
+                    user_field.setText("");
                 }
                 else {
                     result_info.setText("Неверно");
